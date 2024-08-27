1a86d62b38fc6cd5aa6f2e6c45c97b408873aac5
Eshan Dwivedi <79198616+Eshandwivedi13@users.noreply.github.com>
2024-08-27 16:11:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const inputElem = document.getElementById('text');
+const delayElem = document.getElementById('delay');
+const outputDiv = document.getElementById('output');
+const button = document.getElementById('btn');
+
+button.addEventListener('click', (e)=>{
+	console.log(inputElem.value);
+	e.preventDefault();
+	setTimeout(async () => {
+		const pTag = document.createElement('p');
+		pTag.innerText =  inputElem.value;
+		outputDiv.append(pTag);
+    console.log(outputDiv);
+	}, delayElem.innerText);
+})
